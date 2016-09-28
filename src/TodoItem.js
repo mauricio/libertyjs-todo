4c4e2aecec4029a7c8521c6dd824bee8a7544e61
Maurício Linhares <mlinhares@digitalocean.com>
2016-09-28 17:02:11 -0400

    First component

---
+++ src/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <li>
+        I am a TODO item.
+      </li>
+    );
+  }
+}
+
+export default TodoItem;
